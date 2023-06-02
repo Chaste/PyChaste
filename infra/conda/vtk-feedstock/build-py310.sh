37c1fcfbf50f79aa20273ff9a208f50e59a74476
Kwabena N Amponsah <kwabenantim@yahoo.com>
2023-06-02 23:50:49 +0100

    Simpler vtk build

--- infra/conda/vtk-feedstock/build-py310.sh
+++ infra/conda/vtk-feedstock/build-py310.sh
@@ -2,10 +2,9 @@
 
 set -xeo pipefail
 
-export CONFIG='linux_64_build_varianteglffmpeg5python3.10.____cpython'
+export CONFIG='linux_64_python3.10.____cpython'
 export UPLOAD_PACKAGES='False'
 export BUILD_WITH_CONDA_DEBUG='0'
 export DOCKER_IMAGE='quay.io/condaforge/linux-anvil-cos7-x86_64'
-export CPU_COUNT=$(nproc)
 
 ./run_docker_build.sh
