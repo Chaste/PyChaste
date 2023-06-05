5d58533434298969b863ed92da342d07ec7f6032
Kwabena N Amponsah <kwabenantim@yahoo.com>
2023-06-05 08:54:50 +0100

    Fix OpenGL build variant

--- infra/conda/vtk-feedstock/run_docker_build.sh
+++ infra/conda/vtk-feedstock/run_docker_build.sh
@@ -2,17 +2,16 @@
 
 set -xeo pipefail
 
-export RECIPE_ROOT="$( cd "$( dirname "$0" )" >/dev/null && pwd )"
-export RECIPE_NAME=$(basename ${RECIPE_ROOT})
+RECIPE_ROOT="$( cd "$( dirname "$0" )" >/dev/null && pwd )"
 
 docker info
 
-export HOST_USER_ID=$(id -u)
+HOST_USER_ID=$(id -u)
 
-export ARTIFACTS="${RECIPE_ROOT}/build_artifacts"
+ARTIFACTS="${RECIPE_ROOT}/build_artifacts"
 mkdir -p "$ARTIFACTS"
 
-export UPLOAD_PACKAGES="${UPLOAD_PACKAGES:-False}"
+UPLOAD_PACKAGES="${UPLOAD_PACKAGES:-False}"
 
 docker pull "${DOCKER_IMAGE}"
 docker run --rm -it \
