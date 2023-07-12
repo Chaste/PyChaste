683cb218309b89410c5fb402a969acca573d45ab
Kwabena N Amponsah <kwabenantim@yahoo.com>
2023-07-12 15:11:39 +0100

    Fix c_vector typcaster const_name

--- src/PythonUblasObjectConverters.hpp
+++ src/PythonUblasObjectConverters.hpp
@@ -45,51 +45,51 @@ OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 #include <typeinfo>
 
-#define PYBIND11_CVECTOR_TYPECASTER(T, N)                                                   \
-  namespace pybind11                                                                        \
-  {                                                                                         \
-    namespace detail                                                                        \
-    {                                                                                       \
-      template <>                                                                           \
-      struct type_caster<boost::numeric::ublas::c_vector<T, N>>                             \
-      {                                                                                     \
-      public:                                                                               \
-        typedef boost::numeric::ublas::c_vector<T, N> c_vector_##T##_##N##_t;               \
-        PYBIND11_TYPE_CASTER(c_vector_##T##_##N##_t, const_name("c_vector_##T##_##N##_t")); \
-        bool load(handle src, bool convert)                                                 \
-        {                                                                                   \
-          if (!convert && !array_t<T>::check_(src))                                         \
-          {                                                                                 \
-            return false;                                                                   \
-          }                                                                                 \
-          auto buf = array_t<T, array::c_style | array::forcecast>::ensure(src);            \
-          if (!buf)                                                                         \
-          {                                                                                 \
-            return false;                                                                   \
-          }                                                                                 \
-          if (buf.ndim() != 1 or buf.shape()[0] != N)                                       \
-          {                                                                                 \
-            return false;                                                                   \
-          }                                                                                 \
-          value.resize(N);                                                                  \
-          for (unsigned i = 0; i < N; ++i)                                                  \
-          {                                                                                 \
-            value[i] = buf.data()[i];                                                       \
-          }                                                                                 \
-          return true;                                                                      \
-        }                                                                                   \
-        static handle cast(const boost::numeric::ublas::c_vector<T, N> &src,                \
-                           return_value_policy policy,                                      \
-                           handle parent)                                                   \
-        {                                                                                   \
-          std::vector<size_t> shape(1, N);                                                  \
-          std::vector<size_t> strides(1, sizeof(T));                                        \
-          T *data = src.size() ? const_cast<T *>(&src[0]) : static_cast<T *>(NULL);         \
-          array a(std::move(shape), std::move(strides), data);                              \
-          return a.release();                                                               \
-        }                                                                                   \
-      };                                                                                    \
-    }                                                                                       \
+#define PYBIND11_CVECTOR_TYPECASTER(T, N)                                                     \
+  namespace pybind11                                                                          \
+  {                                                                                           \
+    namespace detail                                                                          \
+    {                                                                                         \
+      template <>                                                                             \
+      struct type_caster<boost::numeric::ublas::c_vector<T, N>>                               \
+      {                                                                                       \
+      public:                                                                                 \
+        typedef boost::numeric::ublas::c_vector<T, N> c_vector_##T##_##N##_t;                 \
+        PYBIND11_TYPE_CASTER(c_vector_##T##_##N##_t, const_name("c_vector_" #T "_" #N "_t")); \
+        bool load(handle src, bool convert)                                                   \
+        {                                                                                     \
+          if (!convert && !array_t<T>::check_(src))                                           \
+          {                                                                                   \
+            return false;                                                                     \
+          }                                                                                   \
+          auto buf = array_t<T, array::c_style | array::forcecast>::ensure(src);              \
+          if (!buf)                                                                           \
+          {                                                                                   \
+            return false;                                                                     \
+          }                                                                                   \
+          if (buf.ndim() != 1 or buf.shape()[0] != N)                                         \
+          {                                                                                   \
+            return false;                                                                     \
+          }                                                                                   \
+          value.resize(N);                                                                    \
+          for (unsigned i = 0; i < N; ++i)                                                    \
+          {                                                                                   \
+            value[i] = buf.data()[i];                                                         \
+          }                                                                                   \
+          return true;                                                                        \
+        }                                                                                     \
+        static handle cast(const boost::numeric::ublas::c_vector<T, N> &src,                  \
+                           return_value_policy policy,                                        \
+                           handle parent)                                                     \
+        {                                                                                     \
+          std::vector<size_t> shape(1, N);                                                    \
+          std::vector<size_t> strides(1, sizeof(T));                                          \
+          T *data = src.size() ? const_cast<T *>(&src[0]) : static_cast<T *>(NULL);           \
+          array a(std::move(shape), std::move(strides), data);                                \
+          return a.release();                                                                 \
+        }                                                                                     \
+      };                                                                                      \
+    }                                                                                         \
   }
 
 template <typename T, unsigned N>
