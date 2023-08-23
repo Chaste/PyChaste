43f8f5f5648ab1452f0818849e8d5e6169b16184
Kwabena N Amponsah <kwabenantim@yahoo.com>
2023-08-23 14:03:40 +0100

    conda-build: no docker tty

--- infra/conda/chaste/.scripts/run_docker_build.sh
+++ infra/conda/chaste/.scripts/run_docker_build.sh
@@ -21,17 +21,17 @@ rm -f "${DONE_CANARY}"
 UPLOAD_PACKAGES="${UPLOAD_PACKAGES:-False}"
 
 docker pull "${DOCKER_IMAGE}"
-docker run --rm -it \
-           -v "${RECIPE_ROOT}":/home/conda/recipe_root:rw,z,delegated \
-           -v "${FEEDSTOCK_ROOT}":/home/conda/feedstock_root:rw,z,delegated \
-           -e CONFIG \
-           -e UPLOAD_PACKAGES \
-           -e BUILD_WITH_CONDA_DEBUG \
-           -e CPU_COUNT \
-           -e RECIPE_NAME \
-           -e HOST_USER_ID \
-           "${DOCKER_IMAGE}" \
-           bash \
-           "/home/conda/feedstock_root/${PROVIDER_DIR}/build_steps.sh"
+docker run \
+  -v "${RECIPE_ROOT}":/home/conda/recipe_root:rw,z,delegated \
+  -v "${FEEDSTOCK_ROOT}":/home/conda/feedstock_root:rw,z,delegated \
+  -e CONFIG \
+  -e UPLOAD_PACKAGES \
+  -e BUILD_WITH_CONDA_DEBUG \
+  -e CPU_COUNT \
+  -e RECIPE_NAME \
+  -e HOST_USER_ID \
+  "${DOCKER_IMAGE}" \
+  bash \
+  "/home/conda/feedstock_root/${PROVIDER_DIR}/build_steps.sh"
 
 test -f "${DONE_CANARY}"
--- infra/conda/chaste/build-py310.sh
+++ infra/conda/chaste/build-py310.sh
@@ -6,6 +6,6 @@ export CONFIG='linux_64_python3.10.____cpython'
 export UPLOAD_PACKAGES='False'
 export BUILD_WITH_CONDA_DEBUG='0'
 export DOCKER_IMAGE='quay.io/condaforge/linux-anvil-cos7-x86_64'
-export CPU_COUNT=4
+export CPU_COUNT=2
 
 .scripts/run_docker_build.sh
