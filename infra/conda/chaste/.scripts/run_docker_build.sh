1dc9489ff0540e5f02da33d20fcbaa55736d98b3
Kwabena N Amponsah <kwabenantim@yahoo.com>
2023-08-24 17:39:46 +0100

    Update conda-build scripts

--- infra/conda/chaste/.scripts/run_docker_build.sh
+++ infra/conda/chaste/.scripts/run_docker_build.sh
@@ -8,9 +8,7 @@ PROVIDER_DIR="$(basename $THISDIR)"
 FEEDSTOCK_ROOT="$( cd "$( dirname "$0" )/.." >/dev/null && pwd )"
 RECIPE_ROOT="${FEEDSTOCK_ROOT}/recipe"
 
-docker info
-
-HOST_USER_ID=$(id -u)
+cp "${FEEDSTOCK_ROOT}/LICENSE.txt" "${RECIPE_ROOT}/recipe-scripts-license.txt"
 
 ARTIFACTS="${FEEDSTOCK_ROOT}/build_artifacts"
 mkdir -p "${ARTIFACTS}"
@@ -18,20 +16,22 @@ mkdir -p "${ARTIFACTS}"
 DONE_CANARY="${ARTIFACTS}/conda-forge-build-done-${CONFIG}"
 rm -f "${DONE_CANARY}"
 
-UPLOAD_PACKAGES="${UPLOAD_PACKAGES:-False}"
+DOCKER_IMAGE="${DOCKER_IMAGE:-quay.io/condaforge/linux-anvil-cos7-x86_64}"
+export HOST_USER_ID=$(id -u)
+
+docker info
 
 docker pull "${DOCKER_IMAGE}"
 docker run \
-  -v "${RECIPE_ROOT}":/home/conda/recipe_root:rw,z,delegated \
-  -v "${FEEDSTOCK_ROOT}":/home/conda/feedstock_root:rw,z,delegated \
+  --mount type=bind,source="${RECIPE_ROOT}",target=/home/conda/recipe_root \
+  --mount type=bind,source="${FEEDSTOCK_ROOT}",target=/home/conda/feedstock_root \
   -e CONFIG \
-  -e UPLOAD_PACKAGES \
+  -e CHASTE_GIT_BRANCH \
+  -e PYCHASTE_GIT_BRANCH \
   -e BUILD_WITH_CONDA_DEBUG \
   -e CPU_COUNT \
-  -e RECIPE_NAME \
   -e HOST_USER_ID \
   "${DOCKER_IMAGE}" \
-  bash \
-  "/home/conda/feedstock_root/${PROVIDER_DIR}/build_steps.sh"
+  bash "/home/conda/feedstock_root/${PROVIDER_DIR}/build_steps.sh"
 
 test -f "${DONE_CANARY}"
