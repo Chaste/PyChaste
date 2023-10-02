5273349eed1f54d91fb88ecab44ef3f47b22e755
Kwabena N Amponsah <kwabenantim@yahoo.com>
2023-10-02 14:03:58 +0000

    De-escalate vtk build docker commands

--- infra/conda/vtk/.scripts/run_docker_build.sh
+++ infra/conda/vtk/.scripts/run_docker_build.sh
@@ -8,7 +8,7 @@ PROVIDER_DIR="$(basename $THISDIR)"
 FEEDSTOCK_ROOT="$( cd "$( dirname "$0" )/.." >/dev/null && pwd )"
 RECIPE_ROOT="${FEEDSTOCK_ROOT}/recipe"
 
-sudo -E docker info
+docker info
 
 HOST_USER_ID=$(id -u)
 
@@ -20,18 +20,18 @@ rm -f "${DONE_CANARY}"
 
 UPLOAD_PACKAGES="${UPLOAD_PACKAGES:-False}"
 
-sudo -E docker pull "${DOCKER_IMAGE}"
-sudo -E docker run --rm -it \
-           -v "${RECIPE_ROOT}":/home/conda/recipe_root:rw,z,delegated \
-           -v "${FEEDSTOCK_ROOT}":/home/conda/feedstock_root:rw,z,delegated \
-           -e CONFIG \
-           -e UPLOAD_PACKAGES \
-           -e BUILD_WITH_CONDA_DEBUG \
-           -e CPU_COUNT \
-           -e RECIPE_NAME \
-           -e HOST_USER_ID \
-           "${DOCKER_IMAGE}" \
-           bash \
-           "/home/conda/feedstock_root/${PROVIDER_DIR}/build_steps.sh"
+docker pull "${DOCKER_IMAGE}"
+docker run --rm -it \
+  -v "${RECIPE_ROOT}":/home/conda/recipe_root:rw,z,delegated \
+  -v "${FEEDSTOCK_ROOT}":/home/conda/feedstock_root:rw,z,delegated \
+  -e CONFIG \
+  -e UPLOAD_PACKAGES \
+  -e BUILD_WITH_CONDA_DEBUG \
+  -e CPU_COUNT \
+  -e RECIPE_NAME \
+  -e HOST_USER_ID \
+  "${DOCKER_IMAGE}" \
+  bash \
+  "/home/conda/feedstock_root/${PROVIDER_DIR}/build_steps.sh"
 
 test -f "${DONE_CANARY}"
