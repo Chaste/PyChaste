6fb613dae39969fd2d8c3cdf7713d1fcefa45146
Kwabena N Amponsah <kwabenantim@yahoo.com>
2024-03-12 20:01:54 +0000

    #62 Remove non-standard MutableElement wrappers

--- dynamic/wrappers/mesh/MutableElement1_2.cppwg.hpp
+++
@@ -1,6 +0,0 @@
-#ifndef MutableElement1_2_hpp__pyplusplus_wrapper
-#define MutableElement1_2_hpp__pyplusplus_wrapper
-
-namespace py = pybind11;
-void register_MutableElement1_2_class(py::module &m);
-#endif // MutableElement1_2_hpp__pyplusplus_wrapper
